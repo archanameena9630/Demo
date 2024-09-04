612313baddaa67bffd78f76a04f76bdbbe11e4bd
archana <archanameena9630@gmail.com>
2024-09-04 15:35:48 +0530

    Added some content

---
+++ program2.java
@@ -0,0 +1,14 @@
+public class program2 {
+    public class Arrymix {
+        public static void main(String[] args) {
+            int arr []={ 12,33,44,23,65};
+            int mix=arr[0];
+            for (int i=0;i<arr.length;i++){
+                if(mix>arr[i]){
+                    mix=arr[i];
+                }
+            }
+            System.out.println("value"+mix);
+        }
+    }
+}
